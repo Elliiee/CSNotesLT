2bb0173d696023e6584fca39ef68249b97d5ad82
Elliiee <danyiluozhang@gmail.com>
2020-04-20 21:06:52 -0500

    64

---
+++ DynamicProgramming/src/_64MinimumPathSum.java
@@ -0,0 +1,25 @@
+public class _64MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] dp = new int[n]; // the length of the column
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (j == 0){
+                    dp[j] = dp[j];
+                }
+                else if (i == 0){
+                    dp[j] = dp[j-1];
+                }
+                else {
+                    dp[j] = Math.min(dp[j-1], dp[j]);
+                }
+                dp[j] += grid[i][j];
+            }
+        }
+        return dp[n-1];
+    }
+}
