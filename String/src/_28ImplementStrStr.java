bdd255cab56c3706fda2e65fd7996ee9e02a4cae
Elliiee <danyiluozhang@gmail.com>
2020-04-26 18:50:41 -0500

    28

---
+++ String/src/_28ImplementStrStr.java
@@ -0,0 +1,11 @@
+public class _28ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+        for (int start = 0; start < n - L + 1; start++){
+            if (haystack.substring(start, start + L).equals(needle)){
+                return start;
+            }
+        }
+        return -1;
+    }
+}
