dbbb45633d72bbd00c8e5a89e4ec420f3b04aa49
Elliiee <danyiluozhang@gmail.com>
2020-04-27 23:07:06 -0500

    69

---
+++ BinarySearch/src/_69Sqrt.java
@@ -0,0 +1,22 @@
+public class _69Sqrt {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long num;
+        int left = 2, right = x/2, mid;
+        while (left <= right){
+            mid = left + (right - left) / 2;
+            num = (long) mid * mid;
+            if (num > x){
+                right = mid - 1;
+            }
+            else if (num < x){
+                left = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return right;
+    }
+}
